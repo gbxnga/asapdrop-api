8b38ec41202d85a257511aab8b5d851f2aa84466
gbxnga <iamblizzyy@gmail.com>
2018-09-26 21:55:18 +0100

    added update oauth profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Http\Request;
 use JWTAuth;
 use JWTAuthException;
 
@@ -16,18 +16,18 @@ class UserController extends Controller
         try {
             //$e = compact('email','password');
             //dd($e);
-            if (!$token = JWTAuth::attempt( compact('email','password') )) {
+            if (!$token = JWTAuth::attempt(compact('email', 'password'))) {
                 return response()->json([
                     'response' => 'error',
                     'message' => 'Password or email is invalid',
-                    'token'=>$token
+                    'token' => $token,
                 ]);
-            } 
+            }
         } catch (JWTAuthException $e) {
             return response()->json([
                 'response' => 'error',
                 'message' => 'Token creation failed',
-            ]); 
+            ]);
         }
         return $token;
     }
@@ -39,77 +39,159 @@ class UserController extends Controller
             $token = self::getToken($request->email, $request->password);
             $user->auth_token = $token;
             $user->save();
-            $response = ['success'=>true, 'data'=>$user];           
+            $response = ['success' => true, 'data' => $user];
+        } else {
+            $response = ['success' => false, 'data' => 'Record doesnt exists'];
         }
-        else 
-          $response = ['success'=>false, 'data'=>'Record doesnt exists'];
-      
+
         return response()->json($response, 200);
     }
     public function register(Request $request)
-    { 
+    {
         //return response()->json(["success"=>true,"data"=>["hi"=>"hello"]], 201);
         $payload = [
-            'password'=>\Hash::make($request->password),
-            'email'=>$request->email,
-            'name'=>$request->name,
-            'auth_token'=> ''
+            'password' => \Hash::make($request->password),
+            'email' => $request->email,
+            'name' => $request->name,
+            'auth_token' => '',
         ];
-                  
+
         $user = new \App\User($payload);
-        if ($user->save())
-        {
-            
+        if ($user->save()) {
+
             $token = self::getToken($request->email, $request->password); // generate user token
-            
-            if (!is_string($token))  return response()->json(['success'=>false,'data'=>'Token generation failed'], 201);
-            
+
+            if (!is_string($token)) {
+                return response()->json(['success' => false, 'data' => 'Token generation failed'], 201);
+            }
+
             $user = \App\User::where('email', $request->email)->get()->first();
-            
+
             $user->auth_token = $token; // update user token
-            
+
             $user->save();
-            
-            $response = ['success'=>true, 'data'=>$user];        
+
+            $response = ['success' => true, 'data' => $user];
+        } else {
+            $response = ['success' => false, 'data' => 'Couldnt register user'];
         }
-        else
-            $response = ['success'=>false, 'data'=>'Couldnt register user'];
-        
-        
+
         return response()->json($response, 201);
     }
-    public function register_oauth(Request $request)
-    {  
-        $payload = [
-            'password'=>\Hash::make($request->oauth_id),
-            'email'=>$request->email,
-            'name'=>$request->name,
-            'oauth_uid'=>$request->oauth_id,
-            'oauth_token'=>$request->oauth_token,
-            'oauth_provider'=>$request->oauth_provider,
-            'auth_token'=> ''
-        ];
-                  
-        $user = new \App\User($payload);
-        if ($user->save())
-        {
-            
-            $token = self::getToken($request->email, $request->oauth_id); // generate user token
-            
-            if (!is_string($token))  return response()->json(['success'=>false,'data'=>'Token generation failed'], 201);
-            
-            $user = \App\User::where('email', $request->email)->get()->first();
-            
-            $user->auth_token = $token; // update user token
-            
-            $user->save();
-            
-            $response = ['success'=>true, 'data'=>$user];        
+    private static function verifyToken($access_token)
+    {
+        $fb = new \Facebook\Facebook([
+            'app_id' => '1996180800633192',
+            'app_secret' => '8fb9381750121a22a5e0c12007f57957',
+            'default_graph_version' => 'v2.10',
+            //'default_access_token' => '{access-token}', // optional
+        ]);
+
+        // Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
+        //   $helper = $fb->getRedirectLoginHelper();
+        //   $helper = $fb->getJavaScriptHelper();
+        //   $helper = $fb->getCanvasHelper();
+        //   $helper = $fb->getPageTabHelper();
+
+        try {
+            // Get the \Facebook\GraphNodes\GraphUser object for the current user.
+            // If you provided a 'default_access_token', the '{access-token}' is optional.
+            $response = $fb->get('/me?fields=name,email,id', $access_token);
+        } catch (\Facebook\Exceptions\FacebookResponseException $e) {
+            // When Graph returns an error
+            return 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch (\Facebook\Exceptions\FacebookSDKException $e) {
+            // When validation fails or other local issues
+            return 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
         }
-        else
-            $response = ['success'=>false, 'data'=>'Couldnt register user'];
-        
-        
+
+        $me = $response->getGraphUser();
+        return $me;
+        //print_r($me);
+        //echo 'Logged in as ' . $me->getName();
+        //echo $me->getEmail();
+    }
+    public function register_oauth(Request $request)
+    {
+
+        // check if user is already registered in
+        $user = User::where('oauth_uid', $request->oauth_uid)->get()->first();
+        if ($user) {
+
+            try {
+                // verify auth_token
+                $info = self::verifyToken($request->oauth_token);
+
+                if (!is_object($info)) {
+                    return response()->json(['success' => false, 'data' => 'Facebook Could not verify token','error_message'=>$info], 201);
+                } else {
+                    // update record
+                    $user->password = \Hash::make($info->getId());
+                    $user->oauth_token = $request->oauth_token;
+                    $user->oauth_uid = $info->getId();
+                    $user->email = $info->getEmail();
+                    $user->name = $info->getName();
+
+                    $user->save();
+                    
+
+                    // login and return
+                    $token = self::getToken($user->email, $info->getId()); // generate user token
+                    
+                    if (!is_string($token)) {
+                        return response()->json(['email'=>$info->getEmail(),'id'=>$info->getId(),'user' => $user, 'success' => false, 'data' => 'Token generation failed'], 201);
+                    }
+
+                    $user = \App\User::where('email', $user->email)->get()->first();
+
+                    $user->auth_token = $token; // update user token
+
+                    $user->save();
+
+                    $response = ['success' => true, 'data' => $user];
+
+                }
+
+            } catch (Exception $e) {
+                return response()->json(['success' => false, 'data' => 'Could not verify token'], 201);
+            }
+
+        } else {
+            $payload = [
+                'password' => \Hash::make($request->oauth_uid),
+                'email' => $request->email,
+                'name' => $request->name,
+                'oauth_uid' => $request->oauth_uid,
+                'oauth_token' => $request->oauth_token,
+                'oauth_provider' => $request->oauth_provider,
+                'auth_token' => '',
+                'photo' => $request->photo,
+            ];
+
+            $user = new \App\User($payload);
+            if ($user->save()) {
+
+                $token = self::getToken($request->email, $request->oauth_uid); // generate user token
+
+                if (!is_string($token)) {
+                    return response()->json(['user' => $user, 'success' => false, 'data' => 'Token generation failed'], 201);
+                }
+
+                $user = \App\User::where('email', $request->email)->get()->first();
+
+                $user->auth_token = $token; // update user token
+
+                $user->save();
+
+                $response = ['success' => true, 'data' => $user];
+            } else {
+                $response = ['success' => false, 'data' => 'Couldnt register user'];
+            }
+
+        }
+
         return response()->json($response, 201);
-    } 
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/c', 'UserController@verify');
 Route::get('/migrate', function () {
     $exitCode = Artisan::call('migrate'); 
     return 'DONE'; 
