f851ccda2a8c3babed55193a117184dece6172d8
gbxnga <iamblizzyy@gmail.com>
2019-02-22 22:10:04 +0100

    updated homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ASAPDROP API
                 </div>
 
                 <div class="links">
