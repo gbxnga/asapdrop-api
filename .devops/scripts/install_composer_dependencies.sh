390156e6546f093c4624249a5f2642ad78e84006
gbxnga <iamblizzyy@gmail.com>
2019-02-22 20:31:27 +0100

    updated htaccess

--- .devops/scripts/install_composer_dependencies.sh
+++ .devops/scripts/install_composer_dependencies.sh
@@ -4,4 +4,4 @@ wget https://getcomposer.org/composer.phar # download composer
 php composer.phar install # install all dependencies
 sudo chmod -R 777 /var/www/html/storage # allow access to storage folder
 aws s3 cp s3://asapdrop-api-config-files/.env /var/www/html #update environment file from S3
-#php artisan migrate # migrate to database
+php artisan migrate # migrate to database
